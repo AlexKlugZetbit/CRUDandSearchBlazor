78c695f32b06c3484d2936f1712d4c794c3a6589
DESKTOP-UL9R65A\Alexander <Alexanderrasmussen10@hotmail.com>
2022-11-10 22:46:00 +0100

    first commit

---
+++ MyFirstBlazorProejct2/DataDB/WeatherForecast.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyFirstBlazorProejct2.DataDB
+{
+    public partial class WeatherForecast
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public DateTime? Date { get; set; }
+        public int? TemperatureC { get; set; }
+        public string? Summary { get; set; }
+        public int? TemperatureF { get; set; }
+    }
+}
